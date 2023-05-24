472ae11ea4c21e913f0a7c1e22b29053c3a96747
Leonardo Ledda <leonardoledda@gmail.com>
2023-05-24 15:41:13 +0200

    miatoll: Convert tree part 2
    * Update blobs
    * Add missing product makefile
    * Convert rootdir to Android.bp
    
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ ssos_miatoll.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common ShapeShiftOS stuff.
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+
+# Inherit from miatoll device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+PRODUCT_NAME := ssos_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := SM6250
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
