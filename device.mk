b45d0c710100704fe45f78df82cc0c813ef85099
Leonardo Ledda <leonardoledda@gmail.com>
2023-06-04 12:58:02 +0200

    miatoll: Drop LiveDisplay
    * Fluid doesn't have it
    
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -211,10 +211,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.miatoll
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
